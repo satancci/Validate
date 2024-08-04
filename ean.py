0581c8f34ccb3aa89c5e60fd83fd58c9f74efcb3
ricson <80329621+satancci@users.noreply.github.com>
2024-08-04 00:37:47 -0300

    Delete ean.py

--- ean.py
+++
@@ -1,6 +0,0 @@
-def check(variable):
-    if len(variable) != 13: 
-        return print('Erro: CNPJ com tamanho inválido.')
-    digito = sum(int(variable[i]) * 3 if i%2 else int(variable[i]) for i in range(12))%10
-    digito = 10-digito if digito !=0 else digito
-    return False if variable[12] != str(digito) else True
