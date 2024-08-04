6b609d50d0454265fc105862a245009e3401d290
ricson <80329621+satancci@users.noreply.github.com>
2024-08-04 00:38:10 -0300

    Delete upc.py

--- upc.py
+++
@@ -1,6 +0,0 @@
-def check(upc: str) -> bool:
-    if len(upc) != 12: 
-        return print('Erro: CNPJ com tamanho inválido.')
-    digito = sum(int(upc[i]) * 3 if not i%2 else int(upc[i]) for i in range(11))%10
-    digito = 10-digito if digito !=0 else digito
-    return False if upc[11] != str(digito) else True
